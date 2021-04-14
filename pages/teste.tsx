6e60c4ac7fc10c1a0cb5d72de7f621b7b2daeacf
Domingos Capitango <59874434+AlienDev66@users.noreply.github.com>
2021-04-14 18:39:58 +0100

    :sparkles: Add test configuration.

---
+++ pages/teste.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Pages: React.FC = () => {
+  return (
+    <>
+      <div className="bg-white dark:bg-gray-800">
+        <h1 className="text-gray-900 dark:text-white">Dark mode is here!</h1>
+        <p className="text-gray-600 dark:text-gray-300">Lorem ipsum...</p>
+      </div>
+    </>
+  );
+};
+
+export default Pages;
